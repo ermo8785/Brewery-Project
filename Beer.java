96a76f720f9e4eedc7d8ddf474d13282d2bbeb47
Rafael Basurto <raba3672@colorado.edu>
2023-04-12 14:42:06 -0600

    here we go again...

---
+++ Beer.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList; 
+import java.util.Arrays;
+import java.util.List;
+
+public abstract class Beer {
+    String name;
+    Double cost;
+    Double price;
+    Double alcVol;
+
+    // Will need beer in stock later on
+    int stock;
+
+    Beer(){
+        
+    }
+}
